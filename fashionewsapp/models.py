b99c422d56a6fd235e84d9f140b9d8eea67be018
Vikmah <viktoria.mahrberg@gmail.com>
2022-04-15 10:51:17 +0000

    Add models and trying to fix error with Heroku live site

--- fashionewsapp/models.py
+++ fashionewsapp/models.py
@@ -1,3 +1,48 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+
+STATUS = ((0, "Draft"), (1, "Published"))
+
+
+class Post(models.Model):
+    """
+    Post Model
+    """
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="news_post")
+    updated_on = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+    featured_image = CloudinaryField('image', default='placeholder')
+    excerpt = models.TextField(blank=True)
+    date_published = models.DateTimeField(auto_now_add=True)
+    likes = models.ManyToManyField(User, related_name='news_likes', blank=True)
+
+    class Meta:
+        ordering = ['date_published']
+
+    def __str__(self):
+        return self.title
+
+    def number_of_likes(self):
+        return self.likes.count()
+
+
+class Comment(models.Model):
+    """
+    Comment Model
+    """
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    name = models.CharField(max_length=80)
+    body = models.TextField(max_length=120)
+    date_published = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["date_published"]
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.name}"
+
 
 # Create your models here.
