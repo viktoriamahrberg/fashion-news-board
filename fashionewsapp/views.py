fd4f7b51c9df8fe1f7258598da40b2fb6ca071f4
Vikmah <viktoria.mahrberg@gmail.com>
2022-04-18 12:17:27 +0000

    Add news post to post_detail page

--- fashionewsapp/views.py
+++ fashionewsapp/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Post, Comment
 
 
@@ -8,3 +8,21 @@ class PostList(generic.ListView):
     queryset = Post.objects.order_by("-date_published")
     template_name = 'index.html'
     paginate_by = 4
+
+
+class PostDetail(View):
+
+    def get(self, request, slug, *args, **kwargs):
+        post = get_object_or_404(Post, slug=slug)
+        comments = post.comments.order_by('date_published')
+        # liked = False
+        # if post.likes.filter(id=self.request.user.id).exists()
+        return render (
+            request,
+            "post_detail.html",
+            {
+                "post": post,
+                "comments": comments,
+            }
+        )
+    
